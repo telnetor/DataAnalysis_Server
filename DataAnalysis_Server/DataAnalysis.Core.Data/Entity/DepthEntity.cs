cd2805f81b045c7b27f7f1905894f63484d38197
SmallHan <SmallHan@DESKTOP-VPDVUVR>
2018-05-28 16:58:38 +0800

    更新实体类

---
+++ DataAnalysis_Server/DataAnalysis.Core.Data/Entity/DepthEntity.cs
@@ -0,0 +1,51 @@
+﻿using DataAnalysis.Manipulation.Base;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAnalysis.Core.Data.BitEntity
+{
+    [Table("tb_Depth")]
+    public class DepthEntity
+    {
+        public int Id { get; set; }
+        /// <summary>
+        /// 币名称
+        /// </summary>
+        public string CrrencyName { get; set; }
+        /// <summary>
+        /// 币类型 深度 or KLine
+        /// </summary>
+        public string CrrencyType { get; set; }
+
+        /// <summary>
+        /// 买入总成交量
+        /// </summary>
+        public float TotalBuyVolume { get; set; }
+
+        /// <summary>
+        /// 买入总成交价格
+        /// </summary>
+        public float TotalBuyPrice { get; set; }
+
+        /// <summary>
+        /// 卖出总成交量
+        /// </summary>
+        public float TotalSellingVolume { get; set; }
+
+        /// <summary>
+        /// 卖出总成交价格
+        /// </summary>
+        public float TotalSellingPrice { get; set; }
+
+        /// <summary>
+        /// 时间戳
+        /// </summary>
+        public long ts { get; set; }
+
+
+
+    }
+}
+
